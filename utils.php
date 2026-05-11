6ae9d1720d377a5be9527225130e4a952090270a
davidrnu <david.rnu@gmail.com>
2026-05-12 01:31:02 +0200

    fix dotenv safeLoad

--- utils.php
+++ utils.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+$dotenv->safeLoad();
 
 $dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS']);
 
